801772e3e5a55448abecf413b4c39fbb657c5bc1
Sai Ruthvik <msairuthvik@gmail.com>
2025-08-26 19:16:10 -0500

    project structure and venv add to project

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Sai Ruthvik Munugoti",
+    author_email="msairuthvik@gmail.com",
+    packages=find_packages()
+)
